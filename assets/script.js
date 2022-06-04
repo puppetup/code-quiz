05ab09fd8463a7ae8b0e6ae794ad128c3427e894
puppetup <savinohoshizaki@gmail.com>
2022-06-03 18:03:58 -0700

    deleted functions

--- assets/script.js
+++ assets/script.js
@@ -42,19 +42,3 @@ var myQuestions= [
     
 ];
 
-const startCard = document.querySelector("#start-card");
-const questionCard = document.querySelector("#question-card");
-const scoreCard = document.querySelector("#score-card");
-const leaderboardCard = document.querySelector("#leaderboard-card");
-
-function hideCards() {
-    startCard.setAttribute("hidden", true);
-    questionCard.setAttribute("hidden", true);
-    scoreCard.setAttribute("hidden", true);
-    leaderboardCard.setAttribute("hidden", true);
-  }
-
-  const resultDiv = document.querySelector("#result-div");
-  const resultText = document.querySelector("#result-text");
-
-function 
